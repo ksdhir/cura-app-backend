bafacbd0b7907358687ebcd2199e7224b6787304
Karan Singh Dhir <ksinghdhir1@gmail.com>
2023-10-18 20:24:51 -0700

    add configs to allow cors

--- src/index.ts
+++ src/index.ts
@@ -30,6 +30,12 @@ app.use(
   cors()
 );
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 // Express configuration
 app.use(compression());
 app.use(cookieParser());
